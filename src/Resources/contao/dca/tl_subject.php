dca213c169dfedb72461433bed3682ffa1cb7da0
JTS22 <jonas.schroeder22@hotmail.com>
2018-07-29 17:42:46 +0200

    Fixed save-callback for subject abbreviation

--- src/Resources/contao/dca/tl_subject.php
+++ src/Resources/contao/dca/tl_subject.php
@@ -113,11 +113,12 @@ $GLOBALS['TL_DCA']['tl_subject'] = array
         ),
         'abbreviation' => array(
             'label' => &$GLOBALS['TL_LANG']['tl_subject']['abbreviation'],
-            'exclude' => true,
             'inputType' => 'text',
             'search' => true,
             'eval' => array('maxlenght' => 6, 'tl_class' => 'w50', 'unique' => true),
-            //'save_callback' => array(array('gywaschoolbundle.subjectManager', 'onSubjectAbbrSaved')),
+            'save_callback' => array(
+                array('gywaschoolbundle.subjectManager', 'onSubjectAbbrSaved')
+            ),
             'sql' => "varchar(6) NOT NULL default ''"
         ),
         'referencePage' => array(
@@ -133,7 +134,7 @@ $GLOBALS['TL_DCA']['tl_subject'] = array
             'inputType' => 'select',
             'foreignKey' => 'tl_category.title',
             'eval' => array('mandatory' => true, 'chosen' => true, 'tl_class' => 'w50'),
-            'relation' => array('type'=>'hasOne', 'load'=>'lazy'),
+            'relation' => array('type' => 'hasOne', 'load' => 'lazy'),
             'sql' => "int(10) unsigned NOT NULL default '0'"
         )
     )
