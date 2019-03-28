87c6766c1d0c2b938ca861a501c0070c36c7a3cc
JTS22 <jonas.schroeder22@hotmail.com>
2019-03-28 09:35:39 +0100

    Allowed multiple categories for teachers.

--- src/Resources/contao/dca/tl_teacher.php
+++ src/Resources/contao/dca/tl_teacher.php
@@ -167,10 +167,10 @@ $GLOBALS['TL_DCA']['tl_teacher'] = array
         ),
         'category' => array(
             'label' => $GLOBALS['TL_LANG']['tl_teacher']['category'],
-            'inputType' => 'select',
+            'inputType' => 'checkboxWizard',
             'foreignKey' => 'tl_teacher_category.title',
-            'eval' => array('mandatory' => true, 'chosen' => true, 'tl_class' => 'w50'),
-            'relation' => array('type' => 'hasOne', 'load' => 'lazy'),
+            'eval' => array('mandatory' => true, 'chosen' => true, 'tl_class' => 'clr'),
+            'relation' => array('type' => 'hasMany', 'load' => 'lazy'),
             'sql' => "int(10) unsigned NOT NULL default '0'"
         )
     )
